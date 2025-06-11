c0b0cf65b2f1cb591cb2ba11dfa06017808b19c5
sadaf1 <khan.sadaf0408@gmail.com>
2025-06-11 15:27:42 +0500

    Fixes

--- src/components/AutoCompleteDropdown.jsx
+++ src/components/AutoCompleteDropdown.jsx
@@ -1,100 +1,105 @@
-import React from "react";
+import React, { useCallback } from "react";
 import { Autocomplete, Checkbox, styled } from "@mui/material";
 import CheckBoxIcon from "@mui/icons-material/CheckBox";
 import CheckBoxOutlineBlankIcon from "@mui/icons-material/CheckBoxOutlineBlank";
 import ClearIcon from "@mui/icons-material/Clear";
-import TextFieldCustom from "components/TextFieldCustom";
 import StarIcon from "@mui/icons-material/Star";
+import PropTypes from "prop-types";
+import TextFieldCustom from "./TextFieldCustom";
+
 const checkedIcon = <CheckBoxIcon fontSize="small" />;
 const icon = <CheckBoxOutlineBlankIcon fontSize="small" />;
 
-/**
- * AutoComplete component.
- * @param {Array} options - The options for the autocomplete.
- * @param {boolean} hasselectall - Indicates if "Select all" option should be included.
- * @param {Array} value - The array of values to filter.
- * @param {string} label - The label for the autocomplete.
- * @param {boolean} multiple - Indicates if multiple values can be selected.
- * @param {function} onChange - The function to handle filter changes.
- * @param {boolean} required - Indicates if this values will be required.
- * @param {boolean} error - Indicates if this field show error.
-* @param {boolean} clearIcon - To remove the cross icon or clear button.
- */
+const GroupHeader = styled('div')(({ theme }) => ({
+  position: 'sticky',
+  top: '-8px',
+  padding: '4px 10px',
+  fontWeight: 'bold',
+  fontSize: '12px',
+  color: theme.palette.primary.main,
+  backgroundColor: theme.palette.background.paper,
+  margin: 0,
+}));
+
+const GroupItems = styled('ul')({
+  padding: 0,
+  margin: 0,
+  fontSize: '12px',
+});
+
 const AutoCompleteDropdown = ({
-  options,
-  hasselectall,
-  value,
-  label,
-  multiple,
-  onChange,
-  required,
-  disabled,
-  error,
-  showClearIcon,
-  limitTags,
-  isGroup,
-  className,
-  size,
-  selectAllLabel,
-  helperText
+  options = [],
+  hasselectall = true,
+  value = [],
+  label = '',
+  multiple = true,
+  onChange = () => { },
+  required = false,
+  disabled = false,
+  error = false,
+  showClearIcon = true,
+  limitTags = 2,
+  isGroup = false,
+  className = '',
+  size = 'small',
+  selectAllLabel = 'Select all',
+  helperText = '',
 }) => {
-
-  const GroupHeader = styled('div')(({ theme }) => ({
-    position: 'sticky',
-    top: '-8px',
-    padding: '4px 10px',
-    fontWeight: 'bold',
-    fontSize: '12px',
-    color: '#ff4815',
-    backgroundColor: '#fff',
-    marginTop: 0,
-    marginBottom: 0,
-  }));
-
-  const GroupItems = styled('ul')({
-    padding: 0,
-    marginTop: 0,
-    marginBottom: 0,
-    fontSize: '12px'
-  });
+  const handleChange = useCallback((event, newValue) => {
+    if (multiple && newValue.find((opt) => opt.value === 'Select all')) {
+      onChange(event, options.length === value.length ? [] : options);
+    } else {
+      onChange(event, newValue);
+    }
+  }, [multiple, onChange, options, value]);
 
   return (
     <Autocomplete
       multiple={multiple}
       disableCloseOnSelect={multiple}
       disabled={disabled}
-      className={`border-1 rounded-2 border-secondary  ${className}`}
       options={
         hasselectall && multiple
-          ? [{ label: selectAllLabel ? selectAllLabel : "Select all", value: "Select all" }, ...options]
+          ? [{ label: selectAllLabel, value: 'Select all' }, ...options]
           : options
       }
-      value={multiple ? options?.filter((option) => value?.includes(option?.value)) : typeof value == 'object' && value != null ? value : null}
-      onChange={(event, value) => onChange(event, value)}
-      getOptionLabel={(option) => String(option?.label)}
-      isOptionEqualToValue={(option, value) => option.value == value.value}
-      groupBy={isGroup ? (option) => option.group_value : () => { }}
-      renderOption={
+      value={
         multiple
-          ? (props, option, { selected }) => (
-            <li {...props} key={option.value}>
-              <Checkbox
-                icon={icon}
-                checkedIcon={checkedIcon}
-                checked={
-                  option?.value == "Select all" &&
-                    value?.length == options?.length ? true : selected
-                }
-                color="primary"
-              />
-              {option?.label}
-
-            </li>
-          )
-          : (props, option, { selected }) => <li {...props} key={option.value}>
-            {option?.label}
-            {option?.star ? <StarIcon style={{ marginLeft: 5, color: '#ff4815' }} fontSize="small" /> : null}
+          ? options.filter((option) => value.includes(option?.value)) || []
+          : typeof value === 'object' && value !== null
+            ? value
+            : null
+      }
+      onChange={handleChange}
+      getOptionLabel={(option) => String(option?.label || '')}
+      isOptionEqualToValue={(option, val) => option?.value === val?.value}
+      groupBy={isGroup ? (option) => option.group_value || '' : undefined}
+      renderOption={(props, option, { selected }) =>
+        multiple ? (
+          <li {...props} key={option.value}>
+            <Checkbox
+              icon={icon}
+              checkedIcon={checkedIcon}
+              checked={
+                option.value === 'Select all'
+                  ? value.length === options.length
+                  : selected
+              }
+              color="primary"
+            />
+            {option.label}
           </li>
+        ) : (
+          <li {...props} key={option.value}>
+            {option.label}
+            {option.star && (
+              <StarIcon
+                sx={{ ml: 0.5, color: 'primary.main' }}
+                fontSize="small"
+              />
+            )}
+          </li>
+        )
       }
       renderGroup={(params) => (
         <li key={params.key}>
@@ -105,51 +110,65 @@ const AutoCompleteDropdown = ({
       renderInput={(params) => (
         <TextFieldCustom
           {...params}
-          label={label ? label : ""}
+          label={label}
           fullWidth
           required={required}
           error={error}
           helperText={helperText}
           size={size}
           sx={{
-            marginTop: "6px",
-            marginBottom: "6px",
-            backgroundColor: "white",
-            marginX: 0
+            mt: 0.75,
+            mb: 0.75,
+            bgcolor: 'background.paper',
           }}
         />
       )}
       clearIcon={showClearIcon ? <ClearIcon /> : null}
       sx={{
-        // backgroundColor: "white",
-        color: "white",
-        maxHeight: "100px",
-        overflowY: "scroll",
-        overflowX: "hidden",
-        border: "#ff4815",
+        maxHeight: 100,
+        overflowY: 'auto',
+        overflowX: 'hidden',
+        '& .MuiOutlinedInput-root': {
+          borderColor: error ? 'error.main' : 'grey.500',
+          borderRadius: '8px',
+        },
       }}
       limitTags={limitTags}
+      className={className}
     />
   );
 };
 
-AutoCompleteDropdown.defaultProps = {
-  options: [],
-  hasselectall: true,
-  value: [],
-  label: '',
-  multiple: true,
-  disabled: false,
-  isGroup: false,
-  onChange: () => { },
-  required: false,
-  error: "false",
-  showClearIcon: true,
-  limitTags: 2,
-  className: "",
-  size: "small",
-  selectAllLabel: '',
-  helperText: ''
+AutoCompleteDropdown.propTypes = {
+  options: PropTypes.arrayOf(
+    PropTypes.shape({
+      label: PropTypes.string.isRequired,
+      value: PropTypes.string.isRequired,
+      group_value: PropTypes.string,
+      star: PropTypes.bool,
+    })
+  ),
+  hasselectall: PropTypes.bool,
+  value: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.string),
+    PropTypes.shape({
+      label: PropTypes.string,
+      value: PropTypes.string,
+    }),
+  ]),
+  label: PropTypes.string,
+  multiple: PropTypes.bool,
+  onChange: PropTypes.func,
+  required: PropTypes.bool,
+  disabled: PropTypes.bool,
+  error: PropTypes.bool,
+  showClearIcon: PropTypes.bool,
+  limitTags: PropTypes.number,
+  isGroup: PropTypes.bool,
+  className: PropTypes.string,
+  size: PropTypes.oneOf(['small', 'medium']),
+  selectAllLabel: PropTypes.string,
+  helperText: PropTypes.string,
 };
 
 export default AutoCompleteDropdown;
